0fa70ca7df380ba90ecb1671aba143149d735e38
kevoflash <kelvinmugao@gmail.com>
2024-09-27 02:47:00 -0700

    fix

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,13 +1,6 @@
-# 0-strace_is_your_friend.pp:
-# manifest - fix Apache 500 error by updating the PHP module misconfig
-
- exec { 'fix-wordpress':
-   command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
-     path    => '/usr/local/bin/:/bin/'
-     }
-
-# notify Apache service to reload after PHP module is updated:
-     service { 'apache2':
-       ensure => 'running',
-         notify => Exec['fix-wordpress'],
-         }
+# fix Apache sever error
+exec { 'fix-typo':
+  onlyif  => 'test -e /var/www/html/wp-settings.php',
+  command => "sed -i 's/phpp/php/' /var/www/html/wp-settings.php",
+  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin'
+}
